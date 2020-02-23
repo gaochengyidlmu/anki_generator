25e877b1b660ca1473b7e2f1fc6ea8ccb53d2430
高承毅 <gaochengyidlmu@163.com>
2020-02-24 00:19:24 +0800

    【更新】 模块: 更新 ejs

--- server.js
+++ server.js
@@ -8,8 +8,16 @@ const send = require('koa-send');
 const moment = require('moment');
 const path = require('path');
 const generatorCSV = require('./app.js');
+const views = require('koa-views');
 
 const app = new Koa();
+app.use(
+  views(path.join(__dirname, 'views'), {
+    map: {
+      html: 'ejs',
+    },
+  }),
+);
 const router = new Router();
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -23,8 +31,9 @@ router.get('/', ctx => {
 
 router.get('/index', async ctx => {
   console.log(moment().format('YYYY-MM-DD HH:mm:ss') + ': 获取 index 页面');
-  const res = await readFile('./views/index.html', 'utf8');
-  ctx.body = res;
+  await ctx.render('index.ejs', {
+    subPath,
+  });
 });
 
 // 支持 upload 中间件
@@ -37,7 +46,6 @@ app.use(
       keepExtensions: true, // 保持文件的后缀
       maxFieldsSize: 2 * 1024 * 1024, // 文件上传大小
       onFileBegin: (name, file) => {
-        // console.log(file);
         // 获取文件后缀
         const ext = path.extname(file.name);
         let filename = path.basename(file.name);
@@ -78,15 +86,15 @@ router.post('/api/upload', async ctx => {
 
 router.get('/api/download/:name', async ctx => {
   const name = ctx.params.name;
-  const path = `upload/${name}`;
-  ctx.attachment(path);
-  await send(ctx, path);
+  const filePath = `upload/${name}`;
+  ctx.attachment(filePath);
+  await send(ctx, filePath);
   setTimeout(() => {
-    const filename = path.basename(path);
+    const filename = path.basename(filePath);
     // 如果是范例，则不删除文件，其它的下载完成后，删除文件
-    if (filename === 'example.js') return;
-    fs.unlink(path, () => {
-      console.log('文件' + path + ': 移除成功');
+    if (filename === 'example.xlsx') return;
+    fs.unlink(filePath, () => {
+      console.log('文件' + filePath + ': 移除成功');
     });
   }, 10 * 1000);
 });
