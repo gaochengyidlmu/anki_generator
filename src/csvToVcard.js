678cd2d40533154665503f81c785b99faecf82ec
高承毅 <gaochengyidlmu@163.com>
2020-06-19 21:09:26 +0800

    【更新】 模块: 更新模版支持的 excel

--- src/csvToVcard.js
+++ src/csvToVcard.js
@@ -8,9 +8,12 @@ module.exports = async function(filePath) {
   const excelHandler = new ExcelHandler();
   const data = await excelHandler.import(filePath, {
     columns: [
-      { header: '姓', key: 'lastName', width: 10 },
-      { header: '名', key: 'firstName', width: 10 },
-      { header: '移动电话', key: 'phone', width: 10 },
+      { header: '分配时间', key: 'time', width: 10 },
+      { header: '所属行业', key: 'industry', width: 10 },
+      { header: '姓名', key: 'lastName', width: 10 },
+      { header: '公司', key: 'company', width: 10 },
+      { header: '职位', key: 'profession', width: 10 },
+      { header: '手机', key: 'mobile', width: 10 },
     ],
   });
   let vCardString = '';
@@ -20,8 +23,8 @@ module.exports = async function(filePath) {
     var vCard1 = vCardsJS();
 
     //set basic properties shown before
-    vCard1.firstName = datum.lastName;
-    vCard1.cellPhone = datum.phone;
+    vCard1.firstName = `${datum.time}-${datum.industry}-${datum.lastName}-${datum.company}-${datum.profession}`;
+    vCard1.cellPhone = datum.mobile;
     vCardString += vCard1.getFormattedString() + '\n';
   }
 
